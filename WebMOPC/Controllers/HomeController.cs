cc02be02c80674747d32b3d44a544ce605fe8aab
namvu <namvu@NAM>
2025-04-04 10:12:12 +0700

    xin chào

--- WebMOPC/Controllers/HomeController.cs
+++ WebMOPC/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace WebMOPC.Controllers
 		public StaffsRepository _staffRepo = new StaffsRepository();
 		public PatientsRepository _patienRepo = new PatientsRepository();
 		public DoctorsRepository _doctorRepo = new DoctorsRepository();
-
+		public string abc = "";
 		public HomeController(ILogger<HomeController> logger)
 		{
 			_logger = logger;
